eff5cb0dc9445a1039202369524b4f74351063d4
jiyoonhur <jiyoon1156@gmail.com>
2021-06-02 13:38:25 +0900

    [FEAT] StrokeButton 컴포넌트 및 스토리 추가 #10
    - Stroke 패턴의 버튼을 추가
    - tailwind.config.js borderWidth, boxsizing에 variant 추가

---
+++ src/components/atoms/buttons/StrokeButton.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import StrokeButton, { StrokeButtonPropsType, FontSize } from './StrokeButton';
+
+export default {
+  title: 'Atom/Button/StrokeButton',
+  component: StrokeButton,
+} as Meta;
+
+const Template: Story<StrokeButtonPropsType> = (args) => <StrokeButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: '월',
+};
+
+export const BigText = Template.bind({});
+BigText.args = {
+  label: '월',
+  fontSize: FontSize.XL,
+};
+
+export const LongText = Template.bind({});
+LongText.args = {
+  label: '이것은버튼일까요아닐까요아무도몰라요',
+};
