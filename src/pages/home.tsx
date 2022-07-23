7cb5b2ef3d4346d552e5ed17119927f09b0f5dbb
Sohyun Park <pje1740@gmail.com>
2022-07-23 12:55:41 +0900

    [CHORE] tailwind 버전 업그레이드 (#23)

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import { NextPage } from 'next';
 
+import FillButton from '@/components/atoms/buttons/FillButton';
+
 const HomePage: NextPage = () => {
   const [userName, setUserName] = useState<string>('user');
   return (
     <>
-      <h1>hello, {userName}</h1>
+      <h1 className="text-red-400">hello, {userName}</h1>
+      <FillButton label="hi" onClick={() => console.log('hi')} />
     </>
   );
 };
