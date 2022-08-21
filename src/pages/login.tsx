b58ce86bc126380867b7b85e26193b989351f092
Sohyun Park <pje1740@gmail.com>
2022-08-21 19:37:51 +0900

    [FIX] JWT 토큰 없는 경우 오류 수정 #18

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { GetServerSideProps } from 'next';
 import * as cookie from 'cookie';
+import router from 'next/router';
 
 const ghClientId = '14b259cdaa75f393cf34';
 const ghScopes = 'read:user';
@@ -8,42 +9,47 @@ const gglClientId = '294482240156-ngbk244rak8hme2a9plnspp5k22dnf00.apps.googleus
 const gglScope = 'https://www.googleapis.com/auth/userinfo.email';
 
 interface LoginPageProps {
-  jwtToken: string;
+  jwtToken: string | null;
 }
 
 const LoginPage = ({ jwtToken }: LoginPageProps) => {
-  const [ghRedirectURI, setGhRedirectURI] = useState<string>('');
-  const [gglRedirectURI, setGglRedirectURI] = useState<string>('');
+  const [redirectURI, setRedirectURI] = useState<string>('');
 
   useEffect(() => {
-    // jwtToken 존재 시 메인 화면으로 리디렉트
-    // 유효기간 지난지 체크 필요
-    setGhRedirectURI(`${window.location.origin}/login-callback`);
-    setGglRedirectURI(`${window.location.origin}/login-callback`);
+    // TODO: jwtToken 존재 시 메인 화면으로 리디렉트
+    // 엄밀하게 늘 서버에 요청하여 유효기간 지난지 체크 필요
+    if (jwtToken) {
+      router.push('/home');
+    }
+    setRedirectURI(`${window.location.origin}/login-callback`);
   }, []);
 
   return (
-    <div>
-      <h1>로그인</h1>
-      <a
-        href={`https://github.com/login/oauth/authorize?client_id=${ghClientId}&redirect_uri=${ghRedirectURI}&scope=${ghScopes}&state=github`}
-      >
-        Github으로 로그인하기
-      </a>
-      <br />
-      <a
-        href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=${gglClientId}&redirect_uri=${gglRedirectURI}&response_type=code&scope=${gglScope}&access_type=offline&state=google`}
-      >
-        Google로 로그인하기
-      </a>
-    </div>
+    <>
+      {!jwtToken && (
+        <div>
+          <h1>로그인</h1>
+          <a
+            href={`https://github.com/login/oauth/authorize?client_id=${ghClientId}&redirect_uri=${redirectURI}&scope=${ghScopes}&state=github`}
+          >
+            Github으로 로그인하기
+          </a>
+          <br />
+          <a
+            href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=${gglClientId}&redirect_uri=${redirectURI}&response_type=code&scope=${gglScope}&access_type=offline&state=google`}
+          >
+            Google로 로그인하기
+          </a>
+        </div>
+      )}
+    </>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const cookies = cookie.parse(context.req.headers.cookie as string);
+  const cookies = cookie.parse(context.req.headers.cookie || '');
   return {
-    props: { jwtToken: cookies.jwtToken },
+    props: { jwtToken: cookies.jwtToken || null },
   };
 };
 
